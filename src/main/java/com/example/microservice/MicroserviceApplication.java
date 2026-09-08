b12c7a5cbfd5081c6e56363623d275f7fcaacfd4
Anufrieva Eva <anufrieva.ev@intechs.by>
2026-09-08 11:36:08 +0300

    locale properties added

--- src/main/java/com/example/microservice/MicroserviceApplication.java
+++ src/main/java/com/example/microservice/MicroserviceApplication.java
@@ -1,10 +1,17 @@
 package com.example.microservice;
 
+import jakarta.websocket.Session;
+import org.apache.tomcat.util.descriptor.LocalResolver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import java.beans.BeanProperty;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 @SpringBootApplication
 public class MicroserviceApplication {
@@ -13,6 +20,18 @@ public class MicroserviceApplication {
         SpringApplication.run(MicroserviceApplication.class, args);
     }
     @Bean
+    public LocaleResolver localeResolver(){
+        SessionLocaleResolver localeResolver=new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.US);
+        return localeResolver;
+    }
+    @Bean
+    public ResourceBundleMessageSource messageSource(){
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setUseCodeAsDefaultMessage(true);
+        messageSource.setBasenames("messages");
+        return messageSource;
+    }
 
 
 }
